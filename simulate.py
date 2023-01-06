28c3fe7425cddca647bd129c5c2b52d282deec34
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-01-05 18:55:09 -0600

    Add

--- simulate.py
+++ simulate.py
@@ -1,11 +1,17 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1, 1001):
     p.stepSimulation()
-    time.sleep(1/100000)
+    time.sleep(1/100000000)
     print(i)
 
 p.disconnect()
