49df31e6e01fd04a1c8b78303019c75ab1e64348
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-01-09 21:00:42 -0600

    Add more

--- analyze.py
+++ analyze.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot
 
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
 frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
-targetAngles = numpy.load('data/targetAngles.npy')
+blTargetAngles = numpy.load('data/blTargetAngles.npy')
+flTargetAngles = numpy.load('data/flTargetAngles.npy')
 
 # matplotlib.pyplot.title('Front and Back Leg Sensors')
 # matplotlib.pyplot.plot(backLegSensorValues, label='backLegSensorValues', linewidth=3)
@@ -11,7 +12,10 @@ targetAngles = numpy.load('data/targetAngles.npy')
 # matplotlib.pyplot.legend()
 # matplotlib.pyplot.show()
 
-matplotlib.pyplot.plot(targetAngles, label='targetAngles')
+matplotlib.pyplot.plot(blTargetAngles, label='Back leg motor values', linewidth=4)
+matplotlib.pyplot.plot(flTargetAngles, label='Front leg motor values', linewidth=1)
+
+matplotlib.pyplot.legend()
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
 
--- simulate.py
+++ simulate.py
@@ -18,8 +18,36 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
 
-oldTargetAngles = numpy.linspace(0, 2*numpy.pi, 1000)
-targetAngles = numpy.zeros(1000)
+# back leg
+blOldTargetAngles = numpy.linspace(0, 2*numpy.pi, 1000)
+blTargetAngles = numpy.zeros(1000)
+
+blAmplitude = numpy.pi/4
+blFrequency = 10
+blPhaseOffset = 0
+
+for i in range(0, 1000):
+    blTargetAngles[i] = numpy.sin(blOldTargetAngles[i]) * numpy.pi/4
+    blTargetAngles[i] = blAmplitude * numpy.sin(blFrequency * (blOldTargetAngles[i] + blPhaseOffset))
+    print(blTargetAngles[i]) 
+
+# front leg
+flOldTargetAngles = numpy.linspace(0, 2*numpy.pi, 1000)
+flTargetAngles = numpy.zeros(1000)
+
+flAmplitude = numpy.pi/4
+flFrequency = 10
+flPhaseOffset = 0
+
+for i in range(0, 1000):
+    flTargetAngles[i] = numpy.sin(flOldTargetAngles[i]) * numpy.pi/4
+    flTargetAngles[i] = flAmplitude * numpy.sin(flFrequency * (flOldTargetAngles[i] + flPhaseOffset))
+    print(flTargetAngles[i]) 
+
+# numpy.save('data/blTargetAngles.npy', blTargetAngles)
+# numpy.save('data/flTargetAngles.npy', flTargetAngles)
+
+# exit()
 
 
 for i in range(0, 1000):
@@ -30,10 +58,11 @@ for i in range(0, 1000):
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
     # store motor values
-    targetAngles[i] = numpy.sin(oldTargetAngles[i]) * numpy.pi/4
-    print(targetAngles[i]) 
-    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=targetAngles[i], maxForce=500)
-    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_FrontLeg', controlMode=p.POSITION_CONTROL, targetPosition=targetAngles[i], maxForce=500)
+    # targetAngles[i] = numpy.sin(oldTargetAngles[i]) * numpy.pi/4
+    # targetAngles[i] = amplitude * numpy.sin(frequency * i * phaseOffset)
+    # print(targetAngles[i]) 
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=blTargetAngles[i], maxForce=500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_FrontLeg', controlMode=p.POSITION_CONTROL, targetPosition=flTargetAngles[i], maxForce=500)
     # pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=random.uniform(-math.pi/48, math.pi/48), maxForce=500)
     # pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_FrontLeg', controlMode=p.POSITION_CONTROL, targetPosition=random.uniform(-math.pi/48, math.pi/48), maxForce=500)
     # pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName=b'Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=(-math.pi/4)*random.random(), maxForce=500)
@@ -43,7 +72,5 @@ for i in range(0, 1000):
 
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
 numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
-
-numpy.save('data/targetAngles.npy', targetAngles)
 
 p.disconnect()
