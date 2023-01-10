66bb91237f8c7905b5a3e62afdc0f6566cc6536c
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-01-09 18:50:38 -0600

    Add simulate.py

--- simulate.py
+++ simulate.py
@@ -1,18 +1,26 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
+robotId = p.loadURDF("body.urdf") # indicates which robot you want prepared for simulation
 p.loadSDF("world.sdf")
 
-for i in range(1, 1001):
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(100)
+
+for i in range(0, 100):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegSensorValues)
     time.sleep(1/60)
-    print(i)
+
+numpy.save('data/sensors.npy', backLegSensorValues)
 
 p.disconnect()
