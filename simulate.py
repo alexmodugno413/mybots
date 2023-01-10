3993469847ea9cc27018dc504a6049ab7342c209
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-01-09 19:13:48 -0600

    Add sensors

--- simulate.py
+++ simulate.py
@@ -14,13 +14,16 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
 
 for i in range(0, 100):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     print(backLegSensorValues)
     time.sleep(1/60)
 
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
+numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
 
 p.disconnect()
